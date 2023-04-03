f48b3a262d558c31013df712cb1dd3ae5f305f2a
seb1080 <sblaisfernandez@gmail.com>
2023-04-02 22:07:46 -0400

    feat: format on save isactivated

--- build/index.js
+++ build/index.js
@@ -1,5 +1,6 @@
 "use strict";
 // eslint-disable-next-line no-console
 var OUT_PUT = "Hello world!";
-var newOutput = OUT_PUT + 'Some new world';
-console.log(newOutput);
+var secondVariable = "Web Dev. env is all good :)";
+var array1 = ['value1', 'value2'];
+console.log("".concat(OUT_PUT, ", ").concat(secondVariable));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-console
 
 const OUT_PUT = `Hello world!`;
+const secondVariable = `Web Dev. env. is all good :)`;
 
 const array1 = ['value1', 'value2'];
 
-console.log(OUT_PUT);
+console.log(`${OUT_PUT}, ${secondVariable}`);
